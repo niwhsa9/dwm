71d242352b9bb43a133a400109d9647e9b3339cf
ashwin <ashwingupta2000@gmail.com>
2022-03-08 13:29:33 -0500

    color change

--- config.h
+++ config.h
@@ -20,10 +20,12 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_yellow[]      = "#fff94d";
+static const char col_greenish[]    = "#38ffaf";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_greenish,  col_greenish},
 };
 
 /* tagging */
