02190d349c7306b9dbabd2779483140dc42e6554
ashwin <ashwingupta2000@gmail.com>
2022-03-22 22:38:00 -0400

    update autostart

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,4 @@
 /home/ashwin/Documents/dwm/dwmblocks/dwmblocks &
-/home/ashwin/Documents/dwm/dwmblocks/python-scripts/idle.py &
+python3 /home/ashwin/Documents/dwm/dwmblocks/python-scripts/idle.py &
 feh --bg-scale /home/ashwin/Pictures/aen8xZ.jpg
 
