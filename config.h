e990fd1cc0e51a36ac2f8c3de508fa6e8655d363
Ashwin Gupta <ashwingupta2000@gmail.com>
2022-03-28 00:13:55 -0400

    much better colors

--- config.h
+++ config.h
@@ -25,13 +25,15 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_orange[]      = "#ffa200";
+static const char col_daisy[]       = "#BCA136";
 static const char col_greenish[]    = "#2bcf7d";
-static const char col_pink[]        = "#a64ea2"; //"#c75fc3"; // "#ad027d";
+static const char col_indigo[]      = "#093145"; 
+static const char col_white[]       = "#ffffff"; 
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray2, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_greenish,  col_greenish},
+	[SchemeNorm] = { col_white, col_indigo, col_indigo },
+	[SchemeSel]  = { col_white, col_daisy,  col_daisy},
 };
 
 /* tagging */
